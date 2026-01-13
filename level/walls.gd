ba3288a075e97b692dbc28eef5f0784c3517d943
Tim Moss <tmoss726@gmail.com>
2026-01-13 17:45:45 -0500

    Add debugging statements

--- level/walls.gd
+++ level/walls.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
+@export var debugging = true
 @export var delete_wall_position = -500
-@export var wall_width := 500.0
+@export var wall_width := 200.0
 @export var gap_size := 150.0
 @export var wall_thickness := 50.0
 @export var screen_height := 600.0
@@ -18,11 +19,18 @@ func _process(delta: float) -> void:
 		queue_free()
 
 func create_wall() -> void:
-	# Random gap
+	# Top Gap
+	# Top bar + half of gap size to create top por
+	
 	var half_gap = gap_size / 2
 	var min_gap_y = half_gap + 50
 	var max_gap_y = screen_height - half_gap - 50
 	var gap_center_y = randf_range(min_gap_y, max_gap_y)
+	if(debugging):
+		print("Half Gap: ", half_gap)
+		print("Min Gap Y: ", min_gap_y)
+		print("Max Gap Y: ", max_gap_y)
+		print("Gap Center Y: ", gap_center_y)
 	
 	# Top wall section
 	var top_wall = StaticBody2D.new()
@@ -34,6 +42,9 @@ func create_wall() -> void:
 	top_shape.size = Vector2(wall_thickness, top_height)
 	top_collision.shape = top_shape
 	top_collision.position = Vector2(0, top_y_position)
+	if(debugging):
+		print("Top Height: ", top_height)
+		print("Top Y Position", top_y_position)
 	
 	top_wall.add_child(top_collision)
 	add_child(top_wall)
@@ -49,6 +60,10 @@ func create_wall() -> void:
 	bottom_shape.size = Vector2(wall_thickness, bottom_height)
 	bottom_collision.shape = bottom_shape
 	bottom_collision.position = Vector2(0, bottom_y_position)
+	if(debugging):
+		print("Top Height: ", bottom_start_y)
+		print("Top Y Position", bottom_height)
+		print("Bottom Position Y", bottom_position_y)
 	
 	bottom_wall.add_child(bottom_collision)
 	add_child(bottom_wall)
